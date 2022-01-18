ea142e9b1e68552d32cd5ac9c32332e2bbb95412
zbrachinara <zbrachinara@gmail.com>
2022-01-18 16:04:33 -0500

    added discrete version of unit interface

--- util/unit/Unit.kt
+++ util/unit/Unit.kt
@@ -1,10 +1,10 @@
 package electronvolts.util.unit
 
-fun Int.cunit(): ContinuousUnit<Int> {
+fun Int.dunit(): DiscreteUnit<Int> {
     val i = this
-    return object : ContinuousUnit<Int> {
-        override fun fromRaw(raw: Double) = raw.toInt()
-        override fun raw() = (i.toDouble())
+    return object : DiscreteUnit<Int> {
+        override fun fromRaw(raw: Long) = raw.toInt()
+        override fun raw() = i.toLong()
     }
 }
 
@@ -14,6 +14,24 @@ fun Double.cunit(): ContinuousUnit<Double> {
         override fun fromRaw(raw: Double) = raw
         override fun raw() = i
     }
+}
+
+interface DiscreteUnit<Unit> {
+    fun fromRaw(raw: Long): Unit
+    fun raw(): Long
+
+    // This will allow any unit to be combined with any other unit
+    // kind of flawed design choice, but it's the only way to cleanly implement scaling
+
+    fun <RHS> plus(rhs: DiscreteUnit<RHS>) = fromRaw(this.raw() + rhs.raw())
+
+    fun <RHS> minus(rhs: DiscreteUnit<RHS>) = fromRaw(this.raw() - rhs.raw())
+
+    fun <RHS> times(rhs: DiscreteUnit<RHS>) = fromRaw(this.raw() * rhs.raw())
+
+    fun <RHS> div(rhs: DiscreteUnit<RHS>) = fromRaw(this.raw() / rhs.raw())
+
+    fun zero() = fromRaw(0)
 }
 
 interface ContinuousUnit<Unit> {
