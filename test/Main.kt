454cb1b1c5867ebc5f2f286d83a9a902ac489fec
zbrachinara <zbrachinara@gmail.com>
2022-01-18 07:31:20 -0500

    fix bad practices

--- test/Main.kt
+++ test/Main.kt
@@ -3,7 +3,7 @@ package electronvolts.test
 import electronvolts.statemachine.internal.State
 import electronvolts.statemachine.internal.StateMachine
 import electronvolts.statemachine.internal.StateName
-import junit.framework.Assert.assertEquals
+import org.junit.Assert.assertEquals
 import java.util.*
 
 enum class States : StateName {
@@ -15,30 +15,30 @@ enum class States : StateName {
 fun main() {
 
     val stateMap = HashMap<StateName, State>()
-    stateMap.put(States.START, object : State {
+    stateMap[States.START] = object : State {
         override fun act(): StateName {
             return States.SLAKE
         }
-    })
-    stateMap.put(States.SLAKE, object : State {
+    }
+    stateMap[States.SLAKE] = object : State {
         override fun act(): StateName {
             return States.END
         }
-    })
-    stateMap.put(States.END, object: State {
+    }
+    stateMap[States.END] = object: State {
         override fun act(): StateName? {
             return null
         }
-    })
+    }
 
     val machine = StateMachine(stateMap, States.START, States.values())
 
     assertEquals(machine.currName, States.START)
-    machine.act();
+    machine.act()
     assertEquals(machine.currName, States.SLAKE)
-    machine.act();
+    machine.act()
     assertEquals(machine.currName, States.END)
-    machine.act();
+    machine.act()
     assertEquals(machine.currName, States.END)
 
 }
