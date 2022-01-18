936ad884d3ab11e2e35f4aae107ed4865f24bb6b
zbrachinara <zbrachinara@gmail.com>
2022-01-18 16:36:25 -0500

    add instant class

--- util/unit/Duration.kt
+++ util/unit/Duration.kt
@@ -1,9 +1,5 @@
 package electronvolts.util.unit
 
-private const val SEC_PER_MSC = 0.001
-private const val SEC_PER_MIN = 60.0
-private const val SEC_PER_HOR = 3600.0
-
 class Duration private constructor(
     private val seconds: Double
 ) : ContinuousUnit<Duration> {
