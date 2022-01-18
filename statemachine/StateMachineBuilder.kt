155c7f3a3d24ee8a5bd4369b17f306ad5f2ad9ef
zbrachinara <zbrachinara@gmail.com>
2022-01-18 08:20:31 -0500

    add state machine builder implementation

--- statemachine/StateMachineBuilder.kt
+++ statemachine/StateMachineBuilder.kt
@@ -1,5 +1,18 @@
 package electronvolts.statemachine
 
-class StateMachineBuilder {
+import electronvolts.statemachine.internal.State
+import electronvolts.statemachine.internal.StateName
+
+class StateMachineBuilder<T : StateName>(
+    private val first: T,
+    private val allNames: Array<T>
+) {
+
+    private val stateMap = HashMap<T, State>()
+
+    fun add(name: StateName, state: State): StateMachineBuilder<T> {
+        stateMap[name as T] = state
+        return this
+    }
 
 }
