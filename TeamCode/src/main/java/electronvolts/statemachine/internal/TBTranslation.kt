15ac527061540f2a5f94a4a40ce63931db940bef
zbrachinara <zbrachinara@gmail.com>
2022-01-18 09:30:25 -0500

    added methods to translate between task/behavior and state

---
+++ TeamCode/src/main/java/electronvolts/statemachine/internal/TBTranslation.kt
@@ -0,0 +1,57 @@
+package electronvolts.statemachine.internal
+
+private typealias TaskList = List<Task>
+private typealias BehaviorList = List<Behavior>
+
+fun BehaviorList.initBehaviors() {
+    this.forEach {
+        it.init()
+    }
+}
+
+fun BehaviorList.actBehaviors() {
+    this.forEach {
+        it.act()
+    }
+}
+
+fun BehaviorList.dropBehaviors() {
+    this.forEach {
+        it.drop()
+    }
+}
+
+fun TaskList.initTasks() {
+    this.forEach {
+        it.init()
+    }
+}
+
+fun TaskList.finishedTask(): Task? {
+    return this.find {
+        it.isDone()
+    }
+}
+
+fun taskBehavior(tasks: TaskList, behaviors: BehaviorList): State {
+    return object : State {
+        var init = false
+        override fun act(): StateName? {
+            if (!init) {
+                tasks.initTasks()
+                behaviors.initBehaviors()
+                init = true
+            }
+            return when (val finished = tasks.finishedTask()) {
+                null -> {
+                    behaviors.actBehaviors()
+                    null
+                }
+                else -> {
+                    behaviors.dropBehaviors()
+                    finished.next()
+                }
+            }
+        }
+    }
+}
