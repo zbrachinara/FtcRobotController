215dd36987fd01329736bbdb762d763fb44f5c43
zbrachinara <zbrachinara@gmail.com>
2022-01-20 20:17:15 -0500

    add state factory functions

--- statemachine/StateMachineBuilder.kt
+++ statemachine/StateMachineBuilder.kt
@@ -4,6 +4,13 @@ import electronvolts.statemachine.internal.State
 import electronvolts.statemachine.internal.StateMachine
 import electronvolts.statemachine.internal.StateName
 
+fun <T : StateName> StateMachineBuilder<T>.addBlank(curr: T, next: T?) =
+    this.add(curr, object : State {
+        override fun act(): StateName? = next
+    })
+
+fun <T : StateName> StateMachineBuilder<T>.addEnd(st: T) = this.addBlank(st, null)
+
 class StateMachineBuilder<T : StateName>(
     private val first: T,
     private val allNames: Array<T>
