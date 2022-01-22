f06cc7dbc0287cd1439015ac6aeceb468dea86fa
zbrachinara <zbrachinara@gmail.com>
2022-01-21 20:10:46 -0500

    remove explicit `invoke()` calls

--- TeamCode/src/main/java/electronvolts/gamepad/ButtonAlive.kt
+++ TeamCode/src/main/java/electronvolts/gamepad/ButtonAlive.kt
@@ -6,7 +6,7 @@ import electronvolts.util.unit.Duration
 class ButtonAlive(private val button: DigitalInput, private val start: DigitalInput) {
     private val timer = TimerOnce(Duration.fromMilliseconds(500.0))
 
-    fun isAlive() = if (button.invoke() && start.invoke()) {
+    fun isAlive() = if (button() && start()) {
         timer.init()
         false
     } else {
