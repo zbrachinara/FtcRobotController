3eca867883d0fc677d15667be1ef4a4215a438bd
zbrachinara <zbrachinara@gmail.com>
2022-01-24 11:23:34 -0500

    generify State class

--- Evlib/src/main/java/org/electronvolts/evlib/statemachine/StateMachineBuilder.kt
+++ Evlib/src/main/java/org/electronvolts/evlib/statemachine/StateMachineBuilder.kt
@@ -5,8 +5,8 @@ import org.electronvolts.evlib.statemachine.internal.StateMachine
 import org.electronvolts.evlib.statemachine.internal.StateName
 
 fun <T : StateName> StateMachineBuilder<T>.addBlank(curr: T, next: T?) =
-    this.add(curr, object : State {
-        override fun act(): StateName? = next
+    this.add(curr, object : State<T> {
+        override fun act(): T? = next
     })
 
 fun <T : StateName> StateMachineBuilder<T>.addEnd(st: T) = this.addBlank(st, null)
@@ -16,14 +16,13 @@ class StateMachineBuilder<T : StateName>(
     private val allNames: Array<T>
 ) {
 
-    private val stateMap = HashMap<StateName, State>()
+    private val stateMap = HashMap<T, State<T>>()
 
-    fun add(name: T, state: State): StateMachineBuilder<T> {
+    fun add(name: T, state: State<T>): StateMachineBuilder<T> {
         stateMap[name] = state
         return this
     }
 
-    @Suppress("UNCHECKED_CAST")
-    fun build() = StateMachine(stateMap as Map<T, State>, first, allNames)
+    fun build() = StateMachine(stateMap, first, allNames)
 
 }
