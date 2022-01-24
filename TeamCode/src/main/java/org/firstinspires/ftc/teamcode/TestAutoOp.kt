f8251aa38b1890107fe578decd7134bfa1254f60
zbrachinara <zbrachinara@gmail.com>
2022-01-24 12:09:11 -0500

    added demo of new feature in test code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutoOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutoOp.kt
@@ -4,18 +4,33 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import org.electronvolts.evlib.opmode.AbstractAutoOp
 import org.electronvolts.evlib.statemachine.StateMachineBuilder
 import org.electronvolts.evlib.statemachine.addEnd
+import org.electronvolts.evlib.statemachine.internal.State
 import org.electronvolts.evlib.statemachine.internal.StateMachine
 import org.electronvolts.evlib.statemachine.internal.StateName
 
 enum class TestStates : StateName {
     START,
+    A,
+    B,
+    C,
+    END,
+}
+
+val dummy = object : State<TestStates> {
+    override fun act() = TestStates.END
 }
 
 @Autonomous(name = "Test Auto Kotlin")
 class TestAutoOp : AbstractAutoOp<BlankConfig, TestStates>() {
     override fun buildStates(): StateMachine<TestStates> =
-        StateMachineBuilder<TestStates>(TestStates.START, TestStates.values())
-            .addEnd(TestStates.START)
+        StateMachineBuilder(TestStates.START, TestStates.values())
+            .addSequence(TestStates.START) {
+                it.next(TestStates.A, dummy)
+                    .next(TestStates.B, dummy)
+                    .next(TestStates.C, dummy)
+                    .next(TestStates.END, dummy)
+            }
+            .addEnd(TestStates.END)
             .build()
 
     override fun createRobotCfg(): BlankConfig = BlankConfig(hardwareMap)
