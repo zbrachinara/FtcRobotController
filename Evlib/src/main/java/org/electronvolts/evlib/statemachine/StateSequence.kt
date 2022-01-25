4df4ce7b6de06c6e2b32250fc8df290e7eb3e66c
zbrachinara <zbrachinara@gmail.com>
2022-01-25 09:37:12 -0500

    rename StateSequenceBuilder to StateSequence

--- Evlib/src/main/java/org/electronvolts/evlib/statemachine/StateSequence.kt
+++ Evlib/src/main/java/org/electronvolts/evlib/statemachine/StateSequence.kt
@@ -3,12 +3,12 @@ package org.electronvolts.evlib.statemachine
 import org.electronvolts.evlib.statemachine.internal.State
 import org.electronvolts.evlib.statemachine.internal.StateName
 
-class StateSequenceBuilder<T : StateName> internal constructor(first: T) {
+class StateSequence<T : StateName> internal constructor(first: T) {
 
     internal val sequence = HashMap<T, State<T>>()
     private var tail = first
 
-    fun next(next: T, state: State<T>): StateSequenceBuilder<T> {
+    fun next(next: T, state: State<T>): StateSequence<T> {
         sequence[tail] = object : State<T> {
             override fun act(): T? {
                 return when (state.act()) {
