3eee65e9f9beb7436dc749662e47c1d1d3e63277
zbrachinara <zbrachinara@gmail.com>
2022-01-25 06:57:32 -0500

    relocate tailless definitions to internal package

--- src/main/java/org/electronvolts/evlib/statemachine/Tailless.kt
+++
@@ -1,7 +0,0 @@
-package org.electronvolts.evlib.statemachine
-
-import org.electronvolts.evlib.statemachine.internal.State
-import org.electronvolts.evlib.statemachine.internal.Task
-
-typealias TaillessState<T> = (next: T) -> State<T>
-typealias TaillessTask<T> = (next: T) -> Task<T>
