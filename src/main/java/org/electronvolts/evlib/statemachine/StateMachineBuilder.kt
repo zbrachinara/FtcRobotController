b90787281121f1b164bf17e747e419bef72eb64f
zbrachinara <zbrachinara@gmail.com>
2022-01-26 12:08:15 -0500

    remove redundant type declaration

--- src/main/java/org/electronvolts/evlib/statemachine/StateMachineBuilder.kt
+++ src/main/java/org/electronvolts/evlib/statemachine/StateMachineBuilder.kt
@@ -6,7 +6,7 @@ import org.electronvolts.evlib.statemachine.internal.StateName
 
 fun <T : StateName> StateMachineBuilder<T>.addBlank(curr: T, next: T?) =
     this.add(curr, object : State<T> {
-        override fun act(): T? = next
+        override fun act() = next
     })
 
 fun <T : StateName> StateMachineBuilder<T>.addEnd(st: T) = this.addBlank(st, null)
