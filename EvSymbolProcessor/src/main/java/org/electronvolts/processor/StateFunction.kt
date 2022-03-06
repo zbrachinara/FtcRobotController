fad4841b403b9e4cac23b5043cbd4e6fa54d339f
Eric Yu <zbrachinara@gmail.com>
2022-03-06 15:01:43 -0500

    add file manipulation

--- EvSymbolProcessor/src/main/java/org/electronvolts/processor/StateFunction.kt
+++ EvSymbolProcessor/src/main/java/org/electronvolts/processor/StateFunction.kt
@@ -3,6 +3,8 @@ package org.electronvolts.processor
 import com.google.devtools.ksp.processing.*
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSVisitorVoid
+import java.io.OutputStream
 
 class StateFunctionProcessorProvider : SymbolProcessorProvider {
     override fun create(environment: SymbolProcessorEnvironment) = StateFunctionProcessor(
@@ -22,10 +24,24 @@ class StateFunctionProcessor(
             .getSymbolsWithAnnotation("org.electronvolts.StateFunction")
             .filterIsInstance<KSClassDeclaration>()
 
+        val file = generator.createNewFile(
+            dependencies = Dependencies(false, *resolver.getAllFiles().toList().toTypedArray()),
+            packageName = "org.electronvolts.evlib.statemachine.states",
+            fileName = "StdStates"
+        )
+        file += "package org.electronvolts.evlib.statemachine.states\n"
+
         symbols.forEach {
+            it.accept(StateClassVisitor(file), Unit)
             logger.info(it.qualifiedName.toString())
         }
 
         return emptyList()
+    }
+}
+
+class StateClassVisitor(private val f: OutputStream) : KSVisitorVoid() {
+    override fun visitClassDeclaration(classDeclaration: KSClassDeclaration, data: Unit) {
+        val name = "add" + classDeclaration.simpleName.toString()
     }
 }
