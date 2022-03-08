955deba7c46f45479b82690ecba098ec75647b40
Eric Yu <zbrachinara@gmail.com>
2022-03-08 06:08:54 -0500

    finish open state function generation

--- src/main/java/org/electronvolts/evlib/statemachine/internal/State.kt
+++ src/main/java/org/electronvolts/evlib/statemachine/internal/State.kt
@@ -1,5 +1,15 @@
 package org.electronvolts.evlib.statemachine.internal
 
+fun <T : StateName> asOpenState(state: State<T>): OpenState<T> = { name ->
+    object : State<T> {
+        override fun act(): T? =
+            when (this.act()) {
+                null -> null
+                else -> name
+            }
+    }
+}
+
 interface State<T : StateName> {
     fun act(): T?
 }
