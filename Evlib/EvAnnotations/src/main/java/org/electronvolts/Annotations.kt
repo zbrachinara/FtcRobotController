1b6fd7147cf50f00188f75471cecbcafc0420860
Eric Yu <zbrachinara@gmail.com>
2022-03-10 10:13:12 -0500

    add some documentation to critical classes

--- Evlib/EvAnnotations/src/main/java/org/electronvolts/Annotations.kt
+++ Evlib/EvAnnotations/src/main/java/org/electronvolts/Annotations.kt
@@ -1,5 +1,19 @@
 package org.electronvolts
 
+/**
+ * Used to mark class, function, or constructor declarations for the extending of state machine
+ * classes.
+ *
+ * All classes should inherit from `State<T>`, and all functions (or constructors) should return an
+ * object which inherits from `State<T>`.
+ *
+ * All private declarations are forbidden from being marked.
+ *
+ * If a class is marked for conversion to `StateFunction`, then all public constructors will be
+ * converted. Either only constructors should be marked or the class should be marked. If both the
+ * class and any of its constructors are marked, an error will be presented to the user.
+ *
+ */
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.CONSTRUCTOR)
 @Retention(AnnotationRetention.SOURCE)
 annotation class StateFunction
