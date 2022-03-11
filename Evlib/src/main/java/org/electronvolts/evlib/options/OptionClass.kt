3b9e5f828c4ed763e6b948d582885eb09e7caca0
Eric Yu <zbrachinara@gmail.com>
2022-03-11 13:04:31 -0500

    removed manually implemented type serializer

--- Evlib/src/main/java/org/electronvolts/evlib/options/OptionClass.kt
+++ Evlib/src/main/java/org/electronvolts/evlib/options/OptionClass.kt
@@ -5,14 +5,11 @@ import org.electronvolts.evlib.util.clamp
 
 private typealias Mutator<T> = (GamepadManager, T) -> T?
 
-interface TypeData<T> {
-
-    val default: T
-    val mutator: (GamepadManager, T) -> T?
-    val serializer: Serializer<T>
-
+data class TypeData<T>(
+    val default: T,
+    val mutator: (GamepadManager, T) -> T?,
+) {
     class Builder<T> {
-
         companion object {
             fun int(
                 min: Int,
@@ -27,15 +24,10 @@ interface TypeData<T> {
                         else -> null
                     }
                 }
-                .serializer(object : Serializer<Int> {
-                    override fun toString(o: Int): String = o.toString()
-                    override fun fromString(s: String) = s.toInt()
-                })
         }
 
-        var default: T? = null
-        var mutator: Mutator<T>? = null
-        var serializer: Serializer<T>? = null
+        private var default: T? = null
+        private var mutator: Mutator<T>? = null
 
         fun default(default: T): Builder<T> {
             this.default = default
@@ -47,22 +39,11 @@ interface TypeData<T> {
             return this
         }
 
-        fun serializer(serializer: Serializer<T>): Builder<T> {
-            this.serializer = serializer
-            return this
-        }
-
-        fun build() = object : TypeData<T> {
-            override val default = this@Builder.default!!
-            override val mutator = this@Builder.mutator!!
-            override val serializer = this@Builder.serializer!!
-        }
-
+        fun build() = TypeData(default!!, mutator!!)
     }
 }
 
 interface OptionClass<T> {
-
     companion object {
         fun <T> define(name: String, builder: TypeData.Builder<T>) = object : OptionClass<T> {
             override val typeData = builder.build()
