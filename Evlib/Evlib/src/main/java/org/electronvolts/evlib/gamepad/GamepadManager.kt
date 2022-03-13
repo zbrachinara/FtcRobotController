e9c9962294fd0b6509f1875cb34768f8ac33ec27
Eric Yu <zbrachinara@gmail.com>
2022-03-13 15:46:11 -0400

    changed backing of `GamepadManager` to be on enum maps

--- Evlib/Evlib/src/main/java/org/electronvolts/evlib/gamepad/GamepadManager.kt
+++ Evlib/Evlib/src/main/java/org/electronvolts/evlib/gamepad/GamepadManager.kt
@@ -6,6 +6,18 @@ typealias EvFunction = (Double) -> Double
 
 enum class InitButton { A, B }
 
+enum class GamepadDigital {
+    A, B, X, Y, LEFT_BUMPER, RIGHT_BUMPER, DPAD_UP, DPAD_DOWN, DPAD_LEFT, DPAD_RIGHT,
+    LEFT_STICK_BUTTON, RIGHT_STICK_BUTTON, BACK, START
+}
+
+enum class GamepadAnalog {
+    LEFT_STICK_X, LEFT_STICK_Y, RIGHT_STICK_X, RIGHT_STICK_Y, LEFT_TRIGGER, RIGHT_TRIGGER,
+}
+
+private typealias Digital = GamepadDigital
+private typealias Analog = GamepadAnalog
+
 /**
  * This file was made by the electronVolts, FTC team 7393
  * Date Created: 1/9/16
@@ -17,30 +29,35 @@ enum class InitButton { A, B }
  * Specify nothing for scalingFunction to opt out of joystick scaling
  */
 data class GamepadManager(
-    val a: DigitalInput,
-    val b: DigitalInput,
-    val x: DigitalInput,
-    val y: DigitalInput,
-    val left_bumper: DigitalInput,
-    val right_bumper: DigitalInput,
-    val dpad_up: DigitalInput,
-    val dpad_down: DigitalInput,
-    val dpad_left: DigitalInput,
-    val dpad_right: DigitalInput,
-    val left_stick_button: DigitalInput,
-    val right_stick_button: DigitalInput,
-    val back: DigitalInput,
-    val start: DigitalInput,
-
     //this stores all the wrapped analog inputs
-    val left_stick_x: AnalogInput,
-    val left_stick_y: AnalogInput,
-    val right_stick_x: AnalogInput,
-    val right_stick_y: AnalogInput,
-    val left_trigger: AnalogInput,
-    val right_trigger: AnalogInput,
     private val detectors: MutableList<DigitalInput>,
+
+    private val digital: Map<GamepadDigital, DigitalInput>,
+    private val analog: Map<GamepadAnalog, AnalogInput>,
 ) {
+
+    val a get() = digital[Digital.A]!!
+    val b get() = digital[Digital.B]!!
+    val x get() = digital[Digital.X]!!
+    val y get() = digital[Digital.Y]!!
+    val left_bumper get() = digital[Digital.LEFT_BUMPER]!!
+    val right_bumper get() = digital[Digital.RIGHT_BUMPER]!!
+    val dpad_up get() = digital[Digital.DPAD_UP]!!
+    val dpad_down get() = digital[Digital.DPAD_DOWN]!!
+    val dpad_left get() = digital[Digital.DPAD_LEFT]!!
+    val dpad_right get() = digital[Digital.DPAD_RIGHT]!!
+    val left_stick_button get() = digital[Digital.LEFT_STICK_BUTTON]!!
+    val right_stick_button get() = digital[Digital.RIGHT_STICK_BUTTON]!!
+    val back get() = digital[Digital.BACK]!!
+    val start get() = digital[Digital.START]!!
+
+    val left_stick_x get() = analog[Analog.LEFT_STICK_X]!!
+    val left_stick_y get() = analog[Analog.LEFT_STICK_Y]!!
+    val right_stick_x get() = analog[Analog.RIGHT_STICK_X]!!
+    val right_stick_y get() = analog[Analog.RIGHT_STICK_Y]!!
+    val left_trigger get() = analog[Analog.LEFT_TRIGGER]!!
+    val right_trigger get() = analog[Analog.RIGHT_TRIGGER]!!
+
     companion object {
         fun fromGamepad(
             gamepad: Gamepad,
@@ -67,42 +84,51 @@ data class GamepadManager(
                 }
             }
 
+            //create all the DigitalInput objects
+            val digital_inputs = mapOf(
+                Pair(Digital.A, DigitalInput({ gamepad.a }, at)),
+                Pair(Digital.B, DigitalInput({ gamepad.b }, at)),
+                Pair(Digital.X, DigitalInput({ gamepad.x }, at)),
+                Pair(Digital.Y, DigitalInput({ gamepad.y }, at)),
+                Pair(Digital.LEFT_BUMPER, DigitalInput({ gamepad.left_bumper }, at)),
+                Pair(Digital.RIGHT_BUMPER, DigitalInput({ gamepad.right_bumper }, at)),
+                Pair(Digital.DPAD_UP, DigitalInput({ gamepad.dpad_up }, at)),
+                Pair(Digital.DPAD_DOWN, DigitalInput({ gamepad.dpad_down }, at)),
+                Pair(Digital.DPAD_LEFT, DigitalInput({ gamepad.dpad_left }, at)),
+                Pair(Digital.DPAD_RIGHT, DigitalInput({ gamepad.dpad_right }, at)),
+                Pair(Digital.LEFT_STICK_BUTTON,
+                    DigitalInput({ gamepad.left_stick_button }, at)),
+                Pair(Digital.RIGHT_STICK_BUTTON,
+                    DigitalInput({ gamepad.right_stick_button }, at)),
+                Pair(Digital.BACK, DigitalInput({ gamepad.back }, at)),
+                Pair(Digital.START, DigitalInput({ gamepad.start }, at)),
+            )
+
+            //create all the AnalogInput objects
+            val analog_inputs = mapOf(
+                Pair(Analog.LEFT_STICK_X,
+                    AnalogInput({ gamepad.left_stick_x.toDouble() }, scalingFunction)),
+                Pair(Analog.LEFT_STICK_Y,
+                    AnalogInput({ gamepad.left_stick_y.toDouble() }, scalingFunction)),
+                Pair(Analog.RIGHT_STICK_X,
+                    AnalogInput({ gamepad.right_stick_x.toDouble() }, scalingFunction)),
+                Pair(Analog.RIGHT_STICK_Y,
+                    AnalogInput({ gamepad.right_stick_y.toDouble() }, scalingFunction)),
+                Pair(Analog.LEFT_TRIGGER,
+                    AnalogInput({ gamepad.left_trigger.toDouble() }, scalingFunction)),
+                Pair(Analog.RIGHT_TRIGGER,
+                    AnalogInput({ gamepad.right_trigger.toDouble() }, scalingFunction))
+            )
+
             return GamepadManager(
-                //create all the DigitalInput objects
-                a = DigitalInput({ gamepad.a }, at),
-                b = DigitalInput({ gamepad.b }, at),
-                x = DigitalInput({ gamepad.x }),
-                y = DigitalInput({ gamepad.y }),
-                left_bumper = DigitalInput({ gamepad.left_bumper }),
-                right_bumper = DigitalInput({ gamepad.right_bumper }),
-                dpad_up = DigitalInput({ gamepad.dpad_up }),
-                dpad_down = DigitalInput({ gamepad.dpad_down }),
-                dpad_left = DigitalInput({ gamepad.dpad_left }),
-                dpad_right = DigitalInput({ gamepad.dpad_right }),
-                left_stick_button = DigitalInput({ gamepad.left_stick_button }),
-                right_stick_button = DigitalInput({ gamepad.right_stick_button }),
-                back = DigitalInput({ gamepad.back }),
-                start = DigitalInput({ gamepad.start }, at),
-                //create all the AnalogInput objects
-                left_stick_x = AnalogInput({ gamepad.left_stick_x.toDouble() }, scalingFunction),
-                left_stick_y = AnalogInput({ gamepad.left_stick_y.toDouble() }, scalingFunction),
-                right_stick_x = AnalogInput({ gamepad.right_stick_x.toDouble() }, scalingFunction),
-                right_stick_y = AnalogInput({ gamepad.right_stick_y.toDouble() }, scalingFunction),
-                left_trigger = AnalogInput({ gamepad.left_trigger.toDouble() }, scalingFunction),
-                right_trigger = AnalogInput({ gamepad.right_trigger.toDouble() }, scalingFunction),
+                digital = digital_inputs,
+                analog = analog_inputs,
                 detectors = detectors,
             )
         }
     }
 
-    //TODO: More pragmatic way to do this perhaps..?
-    fun justTriggered(): Boolean {
-        return a.justPressed() || b.justPressed() || x.justPressed() || y.justPressed() ||
-            left_bumper.justPressed() || right_bumper.justPressed() ||
-            dpad_up.justPressed() || dpad_down.justPressed() || dpad_left.justPressed() || dpad_right.justPressed() ||
-            left_stick_button.justPressed() || right_stick_button.justPressed() ||
-            back.justPressed() || start.justPressed()
-    }
+    fun justTriggered() = digital.values.any { btn -> btn.justPressed() }
 
     fun update() {
         for (d in detectors) {
