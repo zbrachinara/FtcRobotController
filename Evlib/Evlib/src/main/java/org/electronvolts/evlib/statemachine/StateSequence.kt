11c4a53f839ce974f68cb167124e148a8815030b
Eric Yu <zbrachinara@gmail.com>
2022-04-26 06:59:40 -0400

    converted multiline comment into doc comment

--- Evlib/Evlib/src/main/java/org/electronvolts/evlib/statemachine/StateSequence.kt
+++ Evlib/Evlib/src/main/java/org/electronvolts/evlib/statemachine/StateSequence.kt
@@ -7,32 +7,38 @@ import org.electronvolts.evlib.statemachine.internal.StateName
 class StateSequenceBuilder<T : StateName> internal constructor() {
     private val sequence = HashMap<T, State<T>>()
 
-    // The `add` function accepts a pair of a name and an open state, but the sequence requires
-    // complete states. Therefore, the pair can't be added into the sequence immediately, and
-    // must be queued. The state is completed when we get the name of the next state, when `add`
-    // is called a second time. A diagram:
-    //
-    // After calling `add(Name_1, State_1)`:
-    //
-    //   ┌──────────┐
-    //   │  Name_1  │
-    //   ├──────────┤
-    //   │ State_1  ┼──────────> Unknown
-    //   └──────────┘
-    //
-    // After calling `add(Name_2, State_2):
-    //
-    //   ┌──────────┐           ┌──────────┐
-    //   │  Name_1  │     ┌─────┼> Name_2  │
-    //   ├──────────┤     │     ├──────────┤
-    //   │ State_1  ┼─────┘     │ State_2  ┼──────────> Unknown
-    //   └──────────┘           └──────────┘
-    //
-    // In the first call to `add` state 1 begins as an open state -- it has to, because we don't
-    // know which state comes next. But in the second call to `add`, we do. Making this work
-    // requires remembering what the previous call to `add` was, which is exactly what `queued`
-    // does. Once the second call to `add` finishes, we move it out of queued, and move the new,
-    // incomplete state, in.
+    /**
+     *The `add` function accepts a pair of a name and an open state, but the sequence requires
+     *complete states. Therefore, the pair can't be added into the sequence immediately, and
+     *must be queued. The state is completed when we get the name of the next state, when `add`
+     *is called a second time. A diagram:
+     *
+     *After calling `add(Name_1, State_1)`:
+     *
+     * ```
+     *┌──────────┐
+     *│  Name_1  │
+     *├──────────┤
+     *│ State_1  ┼──────────> Unknown
+     *└──────────┘
+     * ```
+     *
+     *After calling `add(Name_2, State_2):
+     *
+     * ```
+     *┌──────────┐           ┌──────────┐
+     *│  Name_1  │     ┌─────┼> Name_2  │
+     *├──────────┤     │     ├──────────┤
+     *│ State_1  ┼─────┘     │ State_2  ┼──────────> Unknown
+     *└──────────┘           └──────────┘
+     * ```
+     *
+     *In the first call to `add` state 1 begins as an open state -- it has to, because we don't
+     *know which state comes next. But in the second call to `add`, we do. Making this work
+     *requires remembering what the previous call to `add` was, which is exactly what `queued`
+     *does. Once the second call to `add` finishes, we move it out of queued, and move the new,
+     *incomplete state, in.
+     */
     private var queued: Pair<T, OpenState<T>>? = null
 
     fun add(name: T, state: OpenState<T>): StateSequenceBuilder<T> {
@@ -64,5 +70,5 @@ class StateSequenceBuilder<T : StateName> internal constructor() {
 }
 
 data class StateSequence<T : StateName> internal constructor(
-    internal val sequence: Map<T, State<T>>
+    internal val sequence: Map<T, State<T>>,
 )
