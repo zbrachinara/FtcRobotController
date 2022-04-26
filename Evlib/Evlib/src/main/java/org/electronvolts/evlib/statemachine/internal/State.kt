5244fa48e041cc50c0b429dc8f338c2a19dbeb7a
Eric Yu <zbrachinara@gmail.com>
2022-04-26 16:16:17 -0400

    remove superfluous `asOpenState` function

--- Evlib/Evlib/src/main/java/org/electronvolts/evlib/statemachine/internal/State.kt
+++ Evlib/Evlib/src/main/java/org/electronvolts/evlib/statemachine/internal/State.kt
@@ -1,15 +1,5 @@
 package org.electronvolts.evlib.statemachine.internal
 
-fun <T : StateName> asOpenState(state: State<T>): OpenState<T> = { name ->
-    object : State<T> {
-        override fun act(): T? =
-            when (state.act()) {
-                null -> null
-                else -> name
-            }
-    }
-}
-
 interface State<T : StateName> {
     fun act(): T?
 }
