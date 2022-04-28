40f9f183c8b26e3f07d0bcbe1c80619e97a497cd
Eric Yu <zbrachinara@gmail.com>
2022-04-28 15:23:33 -0400

    added test native function

--- Evlib/Evlib/src/native/src/lib.rs
+++ Evlib/Evlib/src/native/src/lib.rs
@@ -1,8 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use jni::JNIEnv;
+use jni::objects::{JClass, JString};
+use jni::sys::jstring;
+
+#[no_mangle]
+pub extern "system" fn Java_org_electronvolts_evlib_jni_Native_getStr(
+    env: JNIEnv,
+    _: JClass,
+    input: JString,
+) -> jstring {
+    let input: String = env.get_string(input).unwrap().into();
+    let output = env.new_string(format!("Hello, {input}!")).unwrap();
+    output.into_inner()
 }
