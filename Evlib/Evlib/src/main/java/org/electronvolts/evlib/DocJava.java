b6ceb182d42eedb51b1ee46cf6c7601536e67081
Eric Yu <zbrachinara@gmail.com>
2022-06-28 16:42:32 -0400

    affirm that java classes are included in documentation

---
+++ Evlib/Evlib/src/main/java/org/electronvolts/evlib/DocJava.java
@@ -0,0 +1,14 @@
+package org.electronvolts.evlib;
+
+/**
+ * This is a java class that should be documented. It contains syntax that is not handled by
+ * the kdoc tool
+ * <br>
+ * for example, above is a line break
+ *
+ * <p>
+ * This text is surrounded in paragraph tags
+ * </p>
+ */
+public class DocJava {
+}
